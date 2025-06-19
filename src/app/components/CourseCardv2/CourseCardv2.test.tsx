aeab88c5714756fa3a4949cbd10ce0c15f0ec1a4
Moolmancell <richmondclaudebodot@gmail.com>
2025-06-19 22:14:21 +0800

    Add the CourseCardv2 component

---
+++ src/app/components/CourseCardv2/CourseCardv2.test.tsx
@@ -0,0 +1,32 @@
+import { describe, expect, it } from "vitest";
+import { screen } from "@testing-library/dom";
+import '@testing-library/jest-dom';
+import { CourseCardv2 } from "./CourseCardv2";
+import { cleanup, render } from "@testing-library/react";
+import { beforeEach } from "vitest";
+
+describe("CourseCardv2", () => {
+    beforeEach(() => {
+        render(<CourseCardv2 id={1} lessons={10} title="Foundations of User Experience UX Design" progress={23}/>)
+    })
+    it("displays the lessons lessons", () => {
+        expect(screen.getByText("10 Lessons")).toBeInTheDocument();
+    })
+    it("displays course title", () => {
+        expect(screen.getByText("Foundations of User Experience UX Design")).toBeInTheDocument()
+    });
+    it("displays course progress", () => {
+        expect(screen.getByText("23% Completed")).toBeInTheDocument()
+    })
+    it("displays progress bar", () => {
+        expect(screen.getByTestId("progress-bar")).toHaveStyle('width: 23%')
+    })
+    it("displays the continue button when progress is above 0", () => {
+        expect(screen.getByRole("button", {name: "Continue"})).toBeInTheDocument();
+    })
+    it("displays the start button when progress is 0", () => {
+        cleanup()
+        render(<CourseCardv2 id={1} lessons={10} title="Foundations of User Experience UX Design" progress={0}/>)
+        expect(screen.getByRole("button", {name: "Start"})).toBeInTheDocument();
+    })
+})
---
+++ src/app/components/CourseCardv2/CourseCardv2.tsx
@@ -0,0 +1,15 @@
+import { Button } from "../Button/Button"
+
+export function CourseCardv2({id,lessons, title, progress}: {id: number,lessons: number, title: string, progress: number}) {
+    return (
+        <div key={id} data-testid="course-card">
+            <p>{lessons} Lessons</p>
+            <h2>{title}</h2>
+            <div data-testid="progress-bar" style={{width: `${progress}%`}}></div>
+            <p>{progress}% Completed</p>
+            <Button>
+                {progress > 0 ? "Continue" : "Start"}
+            </Button>
+        </div>
+    )
+}
