a07b8d726de083ecdfa804d45354f6afc2f655c6
Moolmancell <richmondclaudebodot@gmail.com>
2025-06-21 10:45:39 +0800

    Add styles to CourseCardv2 component

--- src/app/components/CourseCardv2/CourseCardv2.tsx
+++ src/app/components/CourseCardv2/CourseCardv2.tsx
@@ -1,14 +1,37 @@
 import { Button } from "../Button/Button"
+import Image from "next/image"
+import BookBlueCircle from '@/app/icons/BookBlueCircle.svg'
 
 export function CourseCardv2({id,lessons, title, progress, courseLink}: 
     {id: number,lessons: number, title: string, progress: number, courseLink: string}) {
     return (
-        <div key={id} data-testid="course-card">
-            <p>{lessons} Lessons</p>
-            <h2>{title}</h2>
-            <div data-testid="progress-bar" style={{width: `${progress}%`}}></div>
-            <p>{progress}% Completed</p>
-            <Button type="link" href={courseLink}>
+        <div key={id} data-testid="course-card" 
+            className="flex flex-col justify-between 
+                    min-h-64 box-border p-4 border 
+                    border-zinc-300 rounded-3xl bg-white"
+        >
+            <div className="flex flex-col gap-4">
+                <div className="flex flex-row gap-3 items-center">
+                    <Image className="size-9" src={BookBlueCircle} alt=""></Image>
+                    <p className="text-sm font-semibold">{lessons} Lessons</p>
+                </div>
+
+                <div className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                        <h2 className="font-bold text-xl/snug line-clamp-2 w-full max-h-[52px]">{title}</h2>
+                        <div className="h-0.5 w-full bg-zinc-300 rounded-full"></div>
+                    </div>
+
+                    <div className="flex flex-col gap-3">
+                        <div className="w-full h-2 bg-zinc-200 rounded-3xl">
+                            <div data-testid="progress-bar" className="h-full bg-blue-500 rounded-3xl" style={{width: `${progress}%`}}></div>
+                        </div>
+                        <p className="text-sm font-medium h-[18px]">{progress}% Completed</p>
+                    </div>
+                </div>
+            </div>
+
+            <Button type="link" href={courseLink} className="default-button self-start">
                 {progress > 0 ? "Continue" : "Start"}
             </Button>
         </div>
