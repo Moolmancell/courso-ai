1465edee19ab3a7a724b38fb0b8cc5b8116f0fa0
richmondclaudebodot <richmondclaudebodot@gmail.com>
2025-06-26 10:06:13 +0000

    Add styles to error in RecentHistory

--- src/app/components/RecentHistory/RecentHistory.tsx
+++ src/app/components/RecentHistory/RecentHistory.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState, useCallback } from "react"
 import { Button } from "../Button/Button"
 import { Skeleton } from "./Skeleton"
+import { ArrowPathIcon } from "@heroicons/react/24/outline"
 
 interface History {
     id?: number,
@@ -23,7 +24,8 @@ export function RecentHistory({ userID }: { userID: string }) {
             //remove
             await new Promise(resolve => setTimeout(resolve, 5000));
 
-            const res = await fetch("/RecentHistoryMockAPI.json");
+            //change to real api
+            const res = await fetch("/RecentHistory");
             if (!res.ok) throw new Error("Failed to fetch data");
 
             const json = await res.json();
@@ -60,9 +62,18 @@ export function RecentHistory({ userID }: { userID: string }) {
                 }
             </div>
         ) : error ? (
-            <div data-testid="error-message">
-                Error: {error}
-                <Button onClick={fetchRecentHistory}>Refresh</Button>
+            <div data-testid="error-message" className="flex flex-col gap-2 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.0} stroke="currentColor" className="size-16 text-zinc-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                </svg>
+
+                <span><span className="font-semibold">Error:</span> {error}</span>
+                <Button onClick={fetchRecentHistory} className="default-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                   <p>Refresh</p> 
+                </Button>
             </div>
         ) : history.length === 0 ? (
             <div data-testid="no-recent-history" className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center">No recent history yet.</div>
