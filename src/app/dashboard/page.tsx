748f434c3e0b6021d11363784c1a229c6c8e9b0c
richmondclaudebodot <richmondclaudebodot@gmail.com>
2025-06-26 08:55:42 +0000

    Add lg styles for dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,36 +6,37 @@ import { RecentHistory } from "../components/RecentHistory/RecentHistory"
 
 export default function Dashboard({nameofUser = "User"} : {nameofUser: string}) {
     return (
-        <div>
-            <section className="px-4 mb-12">
+        <div className="max-w-7xl m-auto">
+            <section className="px-4 mb-12 md:px-8">
                 <h1 className="font-bold text-2xl my-4">Hello, {nameofUser}</h1>
-                <div className="flex flex-col gap-2">
-                    <FinishedCourseCard/>
-                    <ActiveCourseCard/>    
+                <div className="flex flex-col gap-2 sm:flex-row sm:w-full">
+                    <div className="sm:max-w-80 sm:w-full"><FinishedCourseCard/></div>
+                    <div className="sm:max-w-80 sm:w-full"><ActiveCourseCard/></div>
                 </div>
             </section>
+            <div className="lg:grid lg:grid-cols-3 lg:px-8 lg:gap-12">
+                <section className="px-4 mb-12 md:px-8 lg:col-span-2 lg:px-0">
+                    <div className="flex flex-row justify-between items-center my-4">
+                        <h1 className="font-medium text-2xl">Recents</h1>
+                        <Link href={"/dashboard/courses"} className="text-sm font-semibold underline">See all courses</Link>
+                    </div>
 
-            <section className="px-4 mb-12">
-                <div className="flex flex-row justify-between items-center my-4">
-                    <h1 className="font-medium text-2xl">Recents</h1>
-                    <Link href={"/dashboard/courses"} className="text-sm font-semibold underline">See all courses</Link>
-                </div>
+                    <div>
+                        <RecentCoursesv2 userID="1234" />                       
+                    </div>
+                </section>
 
-                <div>
-                    <RecentCoursesv2 userID="1234" />                       
-                </div>
-            </section>
+                <section className="px-4 mb-12 md:px-8 lg:px-0">
+                    <div className="flex flex-row justify-between items-center my-4">
+                        <h1 className="font-medium text-2xl">History</h1>
+                        <Link href={"/dashboard/history"} className="text-sm font-semibold underline">See all courses</Link>
+                    </div>
 
-            <section className="px-4 mb-12">
-                <div className="flex flex-row justify-between items-center my-4">
-                    <h1 className="font-medium text-2xl">History</h1>
-                    <Link href={"/dashboard/history"} className="text-sm font-semibold underline">See all courses</Link>
-                </div>
-
-                <div>
-                    <RecentHistory userID="1234" />                     
-                </div>
-            </section>
+                    <div>
+                        <RecentHistory userID="1234" />                     
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
