5d8b27ed3fc5654b0693fc35d229aa6e39c85a0c
richmondclaudebodot <richmondclaudebodot@gmail.com>
2025-06-26 10:12:27 +0000

    Add error styles to RecentCoursesv2

--- src/app/components/RecentCoursesv2/RecentCoursesv2.tsx
+++ src/app/components/RecentCoursesv2/RecentCoursesv2.tsx
@@ -24,7 +24,7 @@ export function RecentCoursesv2({userID}:{userID: string}) {
     const fetchData = useCallback(async () => {
         try {
             setLoading(true);
-            const res = await fetch(`/RecentCoursesMockApi.v2.json`)
+            const res = await fetch(`/RecentCoursesMockApi.v2`)
 
             //simulate loading times (comment or remove in production)
             //await new Promise(resolve => setTimeout(resolve, 1000));
@@ -55,10 +55,19 @@ export function RecentCoursesv2({userID}:{userID: string}) {
                     }
                 </div>
             ) : error ? (
-                <div data-testid="error-message">
-                    Error: {error}
-                    <Button onClick={fetchData}>Refresh</Button>
-                </div>
+                <div data-testid="error-message" className="flex flex-col gap-4 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.8} stroke="currentColor" className="size-32 text-zinc-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                </svg>
+
+                <span><span className="font-semibold">Error:</span> {error}</span>
+                <Button onClick={fetchData} className="default-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                    </svg>
+                   <p>Refresh</p> 
+                </Button>
+            </div>
             ) : course.length === 0 && !loading && !error ? (
                 <div data-testid="no-courses" className="flex flex-col gap-4 min-w-full text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <Image src={NoCourseFound} alt="" className="max-w-32 h-auto self-center m-4" width={128} height={128}></Image>
