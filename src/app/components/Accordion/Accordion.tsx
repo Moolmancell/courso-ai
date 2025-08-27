86189656e9b493fec1ab2b7f5eec032ba1ebca35
richmondclaudebodot <richmondclaudebodot@gmail.com>
2025-08-27 15:56:50 +0000

    Add styles to the Accordion

--- src/app/components/Accordion/Accordion.tsx
+++ src/app/components/Accordion/Accordion.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 type AccordionProps = {
   title: string;
@@ -18,15 +19,24 @@ export function Accordion({ title, children, hasSubtitle, subtitle }: AccordionP
       <button
         data-testid="accordion"
         onClick={() => setIsOpen((prev) => !prev)}
+        className="flex flex-row justify-between px-4 py-3 w-full"
       >
-        <div>
-            {title}
-            {hasSubtitle && <span>{subtitle}</span>}
+        <div className="flex flex-col gap-2 items-start group">
+          <span className="text-base font-semibold group-hover:underline">{title}</span>
+          {hasSubtitle && <span className="text-sm text-zinc-700">{subtitle}</span>}
         </div>
+
+        <ChevronDownIcon className={`w-6 h-6 transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`}/>
       </button>
 
       {/* Accordion content */}
-      {isOpen && <div>{children}</div>}
+      <div
+        className={`px-4 overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-40" : "max-h-0"
+        }`}
+      >
+        <div className="pb-3">{children}</div>
+      </div>
     </div>
   );
 }
