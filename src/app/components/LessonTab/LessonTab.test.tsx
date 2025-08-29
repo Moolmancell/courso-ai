36ab82627c0970d4fee99d2e457bada5956851f8
Richmond <richmondclaudebodot@gmail.com>
2025-08-29 14:01:44 +0800

    Add LessonTab component

---
+++ src/app/components/LessonTab/LessonTab.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import "@testing-library/jest-dom";
+import LessonTab from "./LessonTab";
+
+describe("LessonTab", () => {
+    beforeEach(() => {
+        // Reset any mocks or setup needed before each test
+    });
+
+    it("LessonTab exists", () => {
+        render(<LessonTab status="incomplete"></LessonTab>);
+        expect(screen.getByTestId("lessontab")).toBeInTheDocument();
+    });
+
+    it('renders the correct lesson title', () => {
+        render(<LessonTab status="incomplete">Sample Lesson</LessonTab>);
+        expect(screen.getByText("Sample Lesson")).toBeInTheDocument();
+    });
+    it('renders Completed LessonTab state', () => {
+        render(<LessonTab status="completed">Completed Lesson</LessonTab>);
+        expect(screen.getByTestId("Completed")).toBeInTheDocument();
+    });
+    it('renders Incomplete LessonTab state', () => {
+        render(<LessonTab status="incomplete">Incomplete Lesson</LessonTab>);
+        expect(screen.getByTestId("Incomplete")).toBeInTheDocument();
+    });
+});
---
+++ src/app/components/LessonTab/LessonTab.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BookOpenIcon } from '@heroicons/react/24/outline';
+import { CheckCircleIcon as CheckCircleSolid } from '@heroicons/react/24/solid';
+import { CheckCircleIcon as CheckCircleOutline } from '@heroicons/react/24/outline';
+export default function LessonTab({children, status}: {children?: React.ReactNode, status: 'completed' | 'incomplete'}) {
+    return <div data-testid="lessontab">
+        <div>
+            <BookOpenIcon className="h-5 w-5 text-zinc-500" />
+            <span>{children}</span>
+        </div>
+
+        <div data-testid={status === 'completed' ? 'Completed' : 'Incomplete'}>
+            {status === 'completed' && <CheckCircleSolid className="h-5 w-5 text-green-500" />}
+            {status === 'incomplete' && <CheckCircleOutline className="h-5 w-5 text-zinc-500" />}
+        </div>
+    </div>;
+}
