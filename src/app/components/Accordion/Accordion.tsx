fc0ad8b77c1cde744984a6d3b318796e6d10bc48
Richmond <richmondclaudebodot@gmail.com>
2025-08-29 14:42:03 +0800

    Fix animation and add cursor-pointer to Accordion component

--- src/app/components/Accordion/Accordion.tsx
+++ src/app/components/Accordion/Accordion.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 type AccordionProps = {
@@ -12,6 +12,7 @@ type AccordionProps = {
 
 export function Accordion({ title, children, hasSubtitle, subtitle }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className="group">
@@ -19,21 +20,27 @@ export function Accordion({ title, children, hasSubtitle, subtitle }: AccordionP
       <button
         data-testid="accordion"
         onClick={() => setIsOpen((prev) => !prev)}
-        className="flex flex-row justify-between px-4 py-3 w-full"
+        className="flex flex-row justify-between px-4 py-3 w-full cursor-pointer"
       >
         <div className="flex flex-col gap-2 items-start">
           <span className="text-base font-semibold group-hover:underline">{title}</span>
           {hasSubtitle && <span className="text-sm text-zinc-700">{subtitle}</span>}
         </div>
 
-        <ChevronDownIcon className={`w-6 h-6 transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`}/>
+        <ChevronDownIcon
+          className={`w-6 h-6 transition-transform duration-300 ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
       </button>
 
       {/* Accordion content */}
       <div
-        className={`px-4 overflow-hidden transition-all duration-300 ease-in-out ${
-          isOpen ? "max-h-40" : "max-h-0"
-        }`}
+        ref={contentRef}
+        style={{
+          maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px` : "0px",
+        }}
+        className="px-4 overflow-hidden transition-all duration-300 ease-in-out"
       >
         <div className="pb-3">{children}</div>
       </div>
