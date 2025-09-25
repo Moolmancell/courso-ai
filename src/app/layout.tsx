5013e0a1a43c2ab555599cbad8c2a89272d42438
Richmond <richmondclaudebodot@gmail.com>
2025-09-25 22:01:26 +0800

    Make DM sans as font at /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import "./globals.css";
+import { DM_Sans } from 'next/font/google';
+
+export const dm_sans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap', 
+  variable: '--font-dm-sans', 
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], 
+  style: ['normal', 'italic'],
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+  
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={dm_sans.className}>
       <body>
         {children}
       </body>
